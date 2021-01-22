3036fe445ba493cd13242ee7a1135b87ad291693
Atharva-764 <75909676+Atharva-764@users.noreply.github.com>
2021-01-22 19:38:50 +0530

    Add files via upload
    This is the website until now.

---
+++ VaniVilas School Website/js/main.js
@@ -0,0 +1,11 @@
+function errorBox_1(){
+    window.alert("If you don't want to apply, you can leave this site wihout pressing any button.")
+}
+
+function redirect(){
+    window.location.replace("admi2.html");
+}
+
+function submit() {
+    window.alert("Sucessfully submitted. You will get an mail in 20 minutes.")
+}
